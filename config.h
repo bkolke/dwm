197bb428eb125c2ba594e673742fe669acf5b1cb
ben_k <benk22@pm.me>
2021-06-20 18:29:38 -0700

    battery and volume buttons

--- config.h
+++ config.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -59,12 +60,23 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 static const char *browsercmd[]  = { "brave-browser", NULL };
+static const char *upvol[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+10%",  NULL };
+static const char *downvol[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-10%",  NULL };
+static const char *mutevol[]   = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle",  NULL };
+static const char *brupcmd[] = { "brightnessctl", "set", "+10%", NULL };
+static const char *brdowncmd[] = { "brightnessctl", "set", "10%-", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd} },
+	{ MODKEY,                       XF86XK_MonBrightnessDown,   spawn,          {.v = brdowncmd} },
+	{ MODKEY,                       XF86XK_MonBrightnessUp,     spawn,          {.v = brupcmd} },
+	{ MODKEY,                       XF86XK_AudioMute,    spawn,          {.v = mutevol} },
+	{ MODKEY,                       XF86XK_AudioLowerVolume,    spawn,          {.v = downvol} },
+	{ MODKEY,                       XF86XK_AudioRaiseVolume,    spawn,          {.v = upvol} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
